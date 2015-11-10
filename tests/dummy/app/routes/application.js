500f9767ee7a6bcdcdcd0f587aba43709a41effd
Ben Kiefer <ben.a.kiefer@gmail.com>
2015-11-10 15:29:47 -0600

    add action hook where a user can specify what action to trigger when the enter key is pressed.

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 var ApplicationRoute = Ember.Route.extend({
     model: function() {
         return Ember.Object.create();
+    },
+    setupController: function (controller, model) {
+        controller.set("triggered", false);
+        controller.set("model", model);
     }
 });
 
