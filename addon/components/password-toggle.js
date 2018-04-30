69952c2ceb9c7c04d45113c4be546880b382b4f3
toran billups <toranb@gmail.com>
2018-04-29 19:23:25 -0500

    DEPENDENCY: upgrade to ember v3.1.1

--- addon/components/password-toggle.js
+++ addon/components/password-toggle.js
@@ -1,45 +1,52 @@
-import Ember from 'ember';
+import $ from 'jquery';
+import { get, computed } from '@ember/object';
+import Component from '@ember/component';
 
-var $ = Ember.$;
+export default Component.extend({
+  didInsertElement: function() {
+    this.$('button').on('click', function() {
+      var text = $(this).text();
+      var $input = $(this).parent().find('input');
 
-export default Ember.Component.extend({
-    didInsertElement: function() {
-        this.$('button').on('click', function() {
-            var text = $(this).text();
-            var $input = $(this).parent().find('input');
-
-            if (text === 'Show') {
-                $(this).text('Hide');
-                $input.attr('autocomplete', 'off');
-                $input.attr('autocorrect', 'off');
-                $input.attr('spellcheck', 'off');
-                $input.attr('autocapitalize', 'off');
-                $input.attr('type', 'text');
-            } else if (text === 'Hide') {
-                $(this).text('Show');
-                $input.removeAttr('autocomplete');
-                $input.removeAttr('autocorrect');
-                $input.removeAttr('spellcheck');
-                $input.removeAttr('autocapitalize');
-                $input.attr('type', 'password');
-            }
-        });
-        if(this.get('focus')) {
-            this.$('input').focus();
-        }
-    },
-    keyPress: function(event){
-        if(event.keyCode === 13) {
-            this.sendAction("action");
-        }
-    },
-    wrapperClazz: Ember.computed('wrapperClass', function() {
-        return 'ember-password-toggle-wrapper ' + this.get('wrapperClass');
-    }),
-    buttonClazz: Ember.computed('buttonClass', function() {
-        return 'ember-password-toggle-btn ' + this.get('buttonClass');
-    }),
-    inputClazz: Ember.computed('inputClass', function() {
-        return 'ember-password-toggle-input ' + this.get('inputClass') ;
-    })
+      if (text === 'Show') {
+        $(this).text('Hide');
+        $input.attr('autocomplete', 'off');
+        $input.attr('autocorrect', 'off');
+        $input.attr('spellcheck', 'off');
+        $input.attr('autocapitalize', 'off');
+        $input.attr('type', 'text');
+      } else if (text === 'Hide') {
+        $(this).text('Show');
+        $input.removeAttr('autocomplete');
+        $input.removeAttr('autocorrect');
+        $input.removeAttr('spellcheck');
+        $input.removeAttr('autocapitalize');
+        $input.attr('type', 'password');
+      }
+    });
+    if(get(this, 'focus')) {
+      this.$('input').focus();
+    }
+  },
+  keyPress: function(event){
+    if(event.keyCode === 13) {
+      const actionToBubble = this.get('action');
+      if (typeof actionToBubble === 'string') {
+        /* eslint-disable */
+        this.sendAction('action');
+        /* eslint-enable */
+      } else {
+        actionToBubble();
+      }
+    }
+  },
+  wrapperClazz: computed('wrapperClass', function() {
+    return 'ember-password-toggle-wrapper ' + get(this, 'wrapperClass');
+  }),
+  buttonClazz: computed('buttonClass', function() {
+    return 'ember-password-toggle-btn ' + get(this, 'buttonClass');
+  }),
+  inputClazz: computed('inputClass', function() {
+    return 'ember-password-toggle-input ' + get(this, 'inputClass') ;
+  })
 });
